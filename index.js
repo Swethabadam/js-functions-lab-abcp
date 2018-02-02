5cae18ff45a5b3d9ec4b37595ba12780d4723bd8
SWETHA BADAM <sweth.badam@gmail.com>
2018-02-02 18:33:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // write your code below!
+function happyHolidays(){
+  return "Happy holidays!"
+}
+happyHolidaysTo("you");
+function happyHolidaysTo(name){
+  
+ console.log("Happy holidays, ${name}!")
+}
